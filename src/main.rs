f063ea917a1e63ce0ae68ae28c627bbd33bd2711
Jeong Jinwoo <jwjeong96@gmail.com>
2022-08-25 00:57:01 +0900

    feat(main): read input and create cars vector

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+use crate::car::Car;
+
 mod car;
 mod view;
 
 fn main() {
     println!("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     let car_names: Vec<String> = view::read_car_names_input();
+
+    let mut cars = Vec::new();
+    for car_name in car_names {
+        cars.push(Car::new(car_name.as_str()).unwrap());
+    }
 }
