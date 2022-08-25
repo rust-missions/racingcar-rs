24736ff74780ed2d607cec2019aa1718ee1b28e4
Jeong Jinwoo <jwjeong96@gmail.com>
2022-08-25 13:22:38 +0900

    feat(view): read total rounds input

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod view;
 fn main() {
     println!("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     let car_names: Vec<String> = view::read_car_names_input();
+    println!("{}", view::read_total_rounds_input());
 
     let mut cars = Vec::new();
     for car_name in car_names {
