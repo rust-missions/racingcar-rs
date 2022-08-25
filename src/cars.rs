006fd1dc61eb303ce594e2dcc33b710cde3642f6
Jeong Jinwoo <jwjeong96@gmail.com>
2022-08-25 15:33:44 +0900

    feat(cars): implement Cars struct as first-class collection

---
+++ src/cars.rs
@@ -0,0 +1,32 @@
+use crate::Car;
+use rand::Rng;
+
+pub struct Cars {
+    pub value: Vec<Car>,
+}
+
+impl Cars {
+    pub fn new(car_names: Vec<String>) -> Result<Cars, &'static str> {
+        let mut cars = Vec::new();
+        for car_name in car_names {
+            cars.push(Car::new(car_name.as_str()).unwrap());
+        }
+        Ok(Cars { value: cars })
+    }
+
+    pub fn play_round(self) -> Cars {
+        let mut new_cars = Vec::new();
+        for car in self.value {
+            let random_number = rand::thread_rng().gen_range(0..=9);
+            new_cars.push(car.play(random_number));
+        }
+        Cars { value: new_cars }
+    }
+
+    pub fn calculate_max_distance(&self) -> u32 {
+        self.value.iter()
+            .max_by_key(|car| car.distance)
+            .unwrap()
+            .distance
+    }
+}
