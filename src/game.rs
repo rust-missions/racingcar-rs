d2e4a1d247c2701d57adb5b01165e01d1c660bda
Jeong Jinwoo <jwjeong96@gmail.com>
2022-08-25 15:45:39 +0900

    refactor(game): manage cars as Cars struct instead of Vector

--- src/game.rs
+++ src/game.rs
@@ -1,27 +1,17 @@
-use crate::Car;
-use rand::Rng;
+use crate::cars::Cars;
 
 pub struct Game {
-    pub cars: Vec<Car>,
+    pub cars: Cars,
     pub round: u32,
 }
 
 impl Game {
-    pub fn new(car_names: Vec<String>) -> Game {
-        let mut cars = Vec::new();
-        for car_name in car_names {
-            cars.push(Car::new(car_name.as_str()).unwrap());
-        }
+    pub fn new(cars: Cars) -> Game {
         Game { cars, round: 0 }
     }
 
     pub fn play(self) -> Game {
-        let mut new_cars = Vec::new();
-        for car in self.cars {
-            let random_number = rand::thread_rng().gen_range(0..=9);
-            new_cars.push(car.play(random_number));
-        }
-        Game { cars: new_cars, round: self.round + 1 }
+        Game { cars: self.cars.play_round(), round: self.round + 1 }
     }
 
     pub fn is_over(&self, total_rounds: u32) -> bool {
@@ -29,13 +19,9 @@ impl Game {
     }
 
     pub fn get_winners(&self) -> Vec<String> {
-        let max_distance = &self.cars.iter()
-            .max_by_key(|car| car.distance)
-            .unwrap()
-            .distance;
-
+        let max_distance = &self.cars.calculate_max_distance();
         let mut winners = Vec::new();
-        for car in &self.cars {
+        for car in &self.cars.value {
             if max_distance > &car.distance {
                 continue;
             }
@@ -47,8 +33,8 @@ impl Game {
 
 #[cfg(test)]
 mod tests {
-    use crate::Game;
-    use super::Car;
+    use crate::{Car, Game};
+    use super::Cars;
 
     #[test]
     fn get_winners() {
@@ -56,7 +42,7 @@ mod tests {
         cars.push(Car { name: String::from("w1"), distance: 3 });
         cars.push(Car { name: String::from("w2"), distance: 3 });
         cars.push(Car { name: String::from("loser"), distance: 2 });
-        let game = Game { cars, round: 5 };
+        let game = Game { cars: Cars { value: cars }, round: 5 };
 
         assert_eq!(game.get_winners(), vec!["w1", "w2"]);
     }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,25 @@
 use crate::car::Car;
+use crate::cars::Cars;
 use crate::game::Game;
 
 mod car;
+mod cars;
 mod game;
 mod view;
 
 fn main() {
     println!("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-    let car_names: Vec<String> = view::read_car_names_input();
+    let cars = Cars::new(view::read_car_names_input()).unwrap();
     let total_rounds = view::read_total_rounds_input();
 
-    let mut game = Game::new(car_names);
+    let mut game = Game::new(cars);
     println!("실행결과");
     loop {
         if game.is_over(total_rounds) {
             break;
         }
         game = game.play();
-        for car in &game.cars {
+        for car in &game.cars.value {
             println!("{} : {}", car.name, "-".repeat(car.distance as usize));
         }
         println!();
