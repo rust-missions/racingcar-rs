57ff5c49f2f6ceae167a64a547e2015365527eda
Jeong Jinwoo <jwjeong96@gmail.com>
2022-08-25 17:46:52 +0900

    feat(main): re-request for car names input instead of ending the process

--- src/cars.rs
+++ src/cars.rs
@@ -9,7 +9,11 @@ impl Cars {
     pub fn new(car_names: Vec<String>) -> Result<Cars, &'static str> {
         let mut cars = Vec::new();
         for car_name in car_names {
-            cars.push(Car::new(car_name.as_str()).unwrap());
+            let car = match Car::new(car_name.as_str()) {
+                Ok(car) => car,
+                Err(e) => return Err(e)
+            };
+            cars.push(car);
         }
         Ok(Cars { value: cars })
     }
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,15 @@ mod view;
 
 fn main() {
     println!("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-    let cars = Cars::new(view::read_car_names_input()).unwrap();
+    let cars :Cars = loop {
+        break match Cars::new(view::read_car_names_input()) {
+            Ok(cars) => cars,
+            Err(err) => {
+                eprintln!("{err}");
+                continue;
+            }
+        }
+    };
     let total_rounds = view::read_total_rounds_input();
 
     let mut game = Game::new(cars, total_rounds);
