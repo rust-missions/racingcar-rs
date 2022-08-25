13484afc155af7c42b97779555645a9b5ca02b26
Jeong Jinwoo <jwjeong96@gmail.com>
2022-08-25 14:40:48 +0900

    feat(main): play all rounds and print each result

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
 use crate::car::Car;
+use crate::game::Game;
 
 mod car;
+mod game;
 mod view;
 
 fn main() {
     println!("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     let car_names: Vec<String> = view::read_car_names_input();
-    println!("{}", view::read_total_rounds_input());
+    let total_rounds = view::read_total_rounds_input();
 
-    let mut cars = Vec::new();
-    for car_name in car_names {
-        cars.push(Car::new(car_name.as_str()).unwrap());
+    let mut game = Game::new(car_names);
+    println!("실행결과");
+    loop {
+        if game.is_over(total_rounds) {
+           return
+        }
+        game = game.play();
+        for car in &game.cars {
+            println!("{} : {}", car.name, "-".repeat(car.distance as usize));
+        }
+        println!();
     }
 }
