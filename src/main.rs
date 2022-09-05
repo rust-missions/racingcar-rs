a21e41310e10cf16097bb309db393def9145e3ea
zmrdltl <meenseek5929@naver.com>
2022-09-05 22:48:19 +0900

    feat(round): add calculation per round

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use {
     rand::Rng,
     std::{cmp, fmt::Error, io},
 };
+
 pub fn print_car_name_format() {
     println!("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 }
@@ -32,17 +33,44 @@ pub fn get_total_round_from_input() -> Result<i64, Error> {
     Ok(total_round)
 }
 
+pub fn print_result_per_round(car_names: Vec<String>, dist_info: Vec<String>) {
+    let cars_num: i64 = car_names.len() as i64;
+    for car_num in 0..cars_num {
+        print!(
+            "{} : {}\n",
+            car_names[car_num as usize], dist_info[car_num as usize]
+        );
+    }
+    println!();
+}
+
+pub fn run_round(car_names: Vec<String>, dist_info: &mut Vec<String>) {
+    let mut rng = rand::thread_rng();
+    let cars_num: i64 = car_names.len() as i64;
+    for car_num in 0..cars_num {
+        let rand_num: i64 = rng.gen_range(0..10);
+        if rand_num >= 4 {
+            let s = dist_info[car_num as usize].to_owned() + "-";
+            dist_info[car_num as usize] = s;
+        }
+    }
+}
+
 fn main() {
     print_car_name_format();
     let car_names: Vec<String> = get_car_names_from_input().unwrap();
-    let cars_num: usize = car_names.len();
+    let cars_num: i64 = car_names.len() as i64;
 
     print_total_round_format();
     let total_round: i64 = get_total_round_from_input().unwrap();
 
-    let mut rng = rand::thread_rng();
     let mut dist_info: Vec<String> = Vec::new();
     for _ in 0..cars_num as i64 {
         dist_info.push("".to_owned());
+    }
+
+    for _ in 0..total_round {
+        run_round(car_names.clone(), &mut dist_info);
+        print_result_per_round(car_names.clone(), dist_info.clone());
     }
 }
