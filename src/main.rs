708d581655b2aae78582010beeb0afa6a1e94d3a
Seoyoung Lee <lsyhime@snu.ac.kr>
2022-09-09 00:38:14 +0900

    fix(model): minor bug fix
    1. add mod model to main
    2. change Car method param to mutable ref

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod model;
+
 fn main() {
     // implement your logic
 }
--- src/model.rs
+++ src/model.rs
@@ -10,7 +10,7 @@ impl Car {
         Car { name, distance: 0 }
     }
 
-    fn move_forward(self) {
+    fn move_forward(&mut self) {
         let rand_num = rand::thread_rng().gen_range(0..=10);
         if rand_num >= 4 {
             self.distance += 1;
