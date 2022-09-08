a098240a8ac30f8fd112078496399ad5b2690248
Seoyoung Lee <lsyhime@snu.ac.kr>
2022-09-09 01:56:49 +0900

    feat(game): add run, winner method signature with test

--- src/game.rs
+++ src/game.rs
@@ -9,4 +9,38 @@ impl Game {
     pub fn new(round: i32, cars: Vec<Car>) -> Self {
         Game { round, cars }
     }
+
+    pub fn run(&mut self) {}
+
+    pub fn winners(&self) -> Vec<Car> {
+        vec![Car::new("tmp")]
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use {super::*, crate::Car};
+
+    #[test]
+    fn winners() {
+        let ding = Car {
+            name: "ding".to_string(),
+            distance: 2,
+        };
+        let young = Car {
+            name: "young".to_string(),
+            distance: 0,
+        };
+        let cars = vec![ding, young];
+        let game = Game::new(0, cars);
+        assert_eq!(game.winners(), vec![ding]); // does this test compares two vec properly?
+
+        let hi = Car {
+            name: "hi".to_string(),
+            distance: 2,
+        };
+        let three_cars = vec![ding, hi, young];
+        let game = Game::new(0, three_cars);
+        assert_eq!(game.winner())
+    }
 }
